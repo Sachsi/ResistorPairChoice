d0fa8664fce78f2b85bad5af7d18c0bbe31b8d8f
Tobias, Sachse <tobisachse27@msn.com>
2018-03-31 22:44:56 +0200

    Greated Layout for App.

--- ResistorPairChoice/Form1.Designer.cs
+++ ResistorPairChoice/Form1.Designer.cs
@@ -28,12 +28,161 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.mL_RequiertValue = new MetroFramework.Controls.MetroLabel();
+            this.mB_Start = new MetroFramework.Controls.MetroButton();
+            this.mTB_RequiertValue = new MetroFramework.Controls.MetroTextBox();
+            this.mTB_ResistorValue1 = new MetroFramework.Controls.MetroTextBox();
+            this.mL_ResistorValue = new MetroFramework.Controls.MetroLabel();
+            this.mTB_ResistorValue2 = new MetroFramework.Controls.MetroTextBox();
+            this.SuspendLayout();
+            // 
+            // mL_RequiertValue
+            // 
+            this.mL_RequiertValue.AutoSize = true;
+            this.mL_RequiertValue.Location = new System.Drawing.Point(23, 89);
+            this.mL_RequiertValue.Name = "mL_RequiertValue";
+            this.mL_RequiertValue.Size = new System.Drawing.Size(93, 19);
+            this.mL_RequiertValue.TabIndex = 1;
+            this.mL_RequiertValue.Text = "Requiert Value";
+            // 
+            // mB_Start
+            // 
+            this.mB_Start.Location = new System.Drawing.Point(238, 253);
+            this.mB_Start.Name = "mB_Start";
+            this.mB_Start.Size = new System.Drawing.Size(115, 39);
+            this.mB_Start.TabIndex = 2;
+            this.mB_Start.Text = "Start";
+            this.mB_Start.UseSelectable = true;
+            this.mB_Start.Click += new System.EventHandler(this.mB_Start_Click);
+            // 
+            // mTB_RequiertValue
+            // 
+            // 
+            // 
+            // 
+            this.mTB_RequiertValue.CustomButton.Image = null;
+            this.mTB_RequiertValue.CustomButton.Location = new System.Drawing.Point(100, 1);
+            this.mTB_RequiertValue.CustomButton.Name = "";
+            this.mTB_RequiertValue.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.mTB_RequiertValue.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.mTB_RequiertValue.CustomButton.TabIndex = 1;
+            this.mTB_RequiertValue.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.mTB_RequiertValue.CustomButton.UseSelectable = true;
+            this.mTB_RequiertValue.CustomButton.Visible = false;
+            this.mTB_RequiertValue.Lines = new string[] {
+        "Please enter your Value"};
+            this.mTB_RequiertValue.Location = new System.Drawing.Point(23, 111);
+            this.mTB_RequiertValue.MaxLength = 32767;
+            this.mTB_RequiertValue.Name = "mTB_RequiertValue";
+            this.mTB_RequiertValue.PasswordChar = '\0';
+            this.mTB_RequiertValue.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.mTB_RequiertValue.SelectedText = "";
+            this.mTB_RequiertValue.SelectionLength = 0;
+            this.mTB_RequiertValue.SelectionStart = 0;
+            this.mTB_RequiertValue.ShortcutsEnabled = true;
+            this.mTB_RequiertValue.Size = new System.Drawing.Size(122, 23);
+            this.mTB_RequiertValue.TabIndex = 3;
+            this.mTB_RequiertValue.Text = "Please enter your Value";
+            this.mTB_RequiertValue.UseSelectable = true;
+            this.mTB_RequiertValue.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.mTB_RequiertValue.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // mTB_ResistorValue1
+            // 
+            // 
+            // 
+            // 
+            this.mTB_ResistorValue1.CustomButton.Image = null;
+            this.mTB_ResistorValue1.CustomButton.Location = new System.Drawing.Point(100, 1);
+            this.mTB_ResistorValue1.CustomButton.Name = "";
+            this.mTB_ResistorValue1.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.mTB_ResistorValue1.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.mTB_ResistorValue1.CustomButton.TabIndex = 1;
+            this.mTB_ResistorValue1.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.mTB_ResistorValue1.CustomButton.UseSelectable = true;
+            this.mTB_ResistorValue1.CustomButton.Visible = false;
+            this.mTB_ResistorValue1.Lines = new string[0];
+            this.mTB_ResistorValue1.Location = new System.Drawing.Point(208, 111);
+            this.mTB_ResistorValue1.MaxLength = 32767;
+            this.mTB_ResistorValue1.Name = "mTB_ResistorValue1";
+            this.mTB_ResistorValue1.PasswordChar = '\0';
+            this.mTB_ResistorValue1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.mTB_ResistorValue1.SelectedText = "";
+            this.mTB_ResistorValue1.SelectionLength = 0;
+            this.mTB_ResistorValue1.SelectionStart = 0;
+            this.mTB_ResistorValue1.ShortcutsEnabled = true;
+            this.mTB_ResistorValue1.Size = new System.Drawing.Size(122, 23);
+            this.mTB_ResistorValue1.TabIndex = 5;
+            this.mTB_ResistorValue1.UseSelectable = true;
+            this.mTB_ResistorValue1.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.mTB_ResistorValue1.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // mL_ResistorValue
+            // 
+            this.mL_ResistorValue.AutoSize = true;
+            this.mL_ResistorValue.Location = new System.Drawing.Point(208, 89);
+            this.mL_ResistorValue.Name = "mL_ResistorValue";
+            this.mL_ResistorValue.Size = new System.Drawing.Size(118, 19);
+            this.mL_ResistorValue.TabIndex = 4;
+            this.mL_ResistorValue.Text = "E24 Resistor Values";
+            // 
+            // mTB_ResistorValue2
+            // 
+            // 
+            // 
+            // 
+            this.mTB_ResistorValue2.CustomButton.Image = null;
+            this.mTB_ResistorValue2.CustomButton.Location = new System.Drawing.Point(100, 1);
+            this.mTB_ResistorValue2.CustomButton.Name = "";
+            this.mTB_ResistorValue2.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.mTB_ResistorValue2.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.mTB_ResistorValue2.CustomButton.TabIndex = 1;
+            this.mTB_ResistorValue2.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.mTB_ResistorValue2.CustomButton.UseSelectable = true;
+            this.mTB_ResistorValue2.CustomButton.Visible = false;
+            this.mTB_ResistorValue2.Lines = new string[0];
+            this.mTB_ResistorValue2.Location = new System.Drawing.Point(208, 140);
+            this.mTB_ResistorValue2.MaxLength = 32767;
+            this.mTB_ResistorValue2.Name = "mTB_ResistorValue2";
+            this.mTB_ResistorValue2.PasswordChar = '\0';
+            this.mTB_ResistorValue2.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.mTB_ResistorValue2.SelectedText = "";
+            this.mTB_ResistorValue2.SelectionLength = 0;
+            this.mTB_ResistorValue2.SelectionStart = 0;
+            this.mTB_ResistorValue2.ShortcutsEnabled = true;
+            this.mTB_ResistorValue2.Size = new System.Drawing.Size(122, 23);
+            this.mTB_ResistorValue2.TabIndex = 6;
+            this.mTB_ResistorValue2.UseSelectable = true;
+            this.mTB_ResistorValue2.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.mTB_ResistorValue2.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(376, 315);
+            this.Controls.Add(this.mTB_ResistorValue2);
+            this.Controls.Add(this.mTB_ResistorValue1);
+            this.Controls.Add(this.mL_ResistorValue);
+            this.Controls.Add(this.mTB_RequiertValue);
+            this.Controls.Add(this.mB_Start);
+            this.Controls.Add(this.mL_RequiertValue);
+            this.Name = "Form1";
+            this.Text = "Resistor Pair Choice";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private MetroFramework.Controls.MetroLabel mL_RequiertValue;
+        private MetroFramework.Controls.MetroButton mB_Start;
+        private MetroFramework.Controls.MetroTextBox mTB_RequiertValue;
+        private MetroFramework.Controls.MetroTextBox mTB_ResistorValue1;
+        private MetroFramework.Controls.MetroLabel mL_ResistorValue;
+        private MetroFramework.Controls.MetroTextBox mTB_ResistorValue2;
     }
 }
 
